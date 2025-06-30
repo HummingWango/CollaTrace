481598e757f501e597421e13f39dc95eca8806b7
HummingWango <176415191+HummingWango@users.noreply.github.com>
2025-06-29 20:42:20 -0400

    learned how to create a canvas using the p5js library
    fixed some quick html issues such as redundant containers.

--- trace.js
+++ trace.js
@@ -3,3 +3,20 @@ let currentBrushSize = 5;
 let p5Canvas;
 let eraserActive = false;
 
+//coordinates for the strokes of drawing
+    let x = 0;
+    let y = 0;
+
+//creating the canvas and other buttons functionality
+function setUp() {
+
+    //how to display flex? can't.
+    const canvasWidth = Math.min(window.innerWidth*0.9, 1000);
+    const canvasHeight = Math.min(window.innerHeight*0.9, 1000);
+
+    p5Canvas = createCanvas(canvasWidth, canvasHeight);
+    p5Canvas.parent(document.body);
+
+    background(255);
+    //created the actual canvas, placed where?
+}
